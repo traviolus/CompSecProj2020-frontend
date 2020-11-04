59d1403fe160bec1fcc14f0dc2cd236eade303bd
armykongtap <army_kongtap@hotmail.com>
2020-11-04 17:09:04 +0700

    Update home page

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,159 +1,51 @@
 import React from "react";
-import { Form, Button, InputGroup, FormControl } from "react-bootstrap";
-import CopyToClipboard from "react-copy-to-clipboard";
-
-import shorten from "api/shorten";
+import { ListGroup } from "react-bootstrap";
 
 import "styles/Home.scss";
 
-interface State {
-  shortenData: {
-    link_original: string;
-  };
-  link_shorten: string;
-  isShorten: boolean;
-  isLoading: boolean;
-  isValidated: boolean;
-  isShortenFailed: boolean;
-  isCopied: boolean;
+interface Topic {
+  topic_id: number;
+  topic_header: string;
+  topic_created_user: string;
+  topic_created_datetime: Date;
 }
 
 class Home extends React.Component {
-  state: State = {
-    shortenData: {
-      link_original: "",
+  data: Array<Topic> = [
+    {
+      topic_id: 1,
+      topic_header: "How to hack facebook",
+      topic_created_user: "armykongtap",
+      topic_created_datetime: new Date("2019-01-16 22:00:00"),
     },
-    link_shorten: "",
-    isShorten: false,
-    isLoading: false,
-    isValidated: false,
-    isShortenFailed: false,
-    isCopied: false,
-  };
-
-  handleInputDataChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = target;
-    const shortenData = this.state.shortenData;
-    if (name === "link_original") {
-      shortenData[name] = value;
-    }
-    this.setState({ shortenData });
-  };
-
-  handleShorten = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    this.setState({ isLoading: true });
-    this.setState({ 
-      isShorten: false, 
-      link_shorten: "", 
-      isValidated: false,
-      isCopied: false,
-      isShortenFailed: false
-    });
-
-    const form = e.currentTarget;
-
-    if (form.checkValidity() === false) {
-      this.setState({ isValidated: true });
-    } else {
-      this.setState({ isShortenFailed: false, isValidated: false });
-
-      try {
-        const result = await shorten(this.state.shortenData);
-        if (result === "") {
-          console.log("Shorten failed!!!");
-          this.setState({ isShortenFailed: true });
-        } else if (result === "incorrect") {
-          console.log("Please enter a correct link!!!");
-          this.setState({ isShortenFailed: true });
-        } else {
-          console.log("Shorten success!!!");
-          this.setState({ isShorten: true });
-          this.setState({
-            link_shorten: process.env.REACT_APP_FRONTEND_URL + result,
-          });
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    this.setState({ isLoading: false });
-  };
-
-  copyToClipboard = () => {
-    console.log("Copy success!!!")
-    this.setState({ isCopied: true });
-  };
+    {
+      topic_id: 2,
+      topic_header: "How to hack ig",
+      topic_created_user: "armykongtap",
+      topic_created_datetime: new Date("2019-01-16 23:00:00"),
+    },
+  ];
 
   render() {
     return (
       <div className="home-container">
         <div className="card-container">
           <div className="main-card">
-            <Form
-              noValidate
-              validated={this.state.isValidated}
-              onSubmit={this.handleShorten}
-            >
-              <InputGroup className="mb-3">
-                <FormControl
-                  required
-                  type="link_original"
-                  placeholder="Shorten your link"
-                  name="link_original"
-                  aria-label="Shorten your link"
-                  aria-describedby="basic-addon2"
-                  onChange={this.handleInputDataChange}
-                />
-                <InputGroup.Append>
-                  <Button
-                    variant="outline-primary"
-                    type="submit"
-                    disabled={this.state.isLoading}
-                  >
-                    Shorten
-                  </Button>
-                </InputGroup.Append>
-
-                <Form.Control.Feedback type="invalid">
-                  Please enter link
-                </Form.Control.Feedback>
-              </InputGroup>
-              { this.state.isShortenFailed ? (
-                <Form.Text className="text-danger">
-                  Please enter a correct link.
-                </Form.Text>
-              ) : (
-                ""
-              )}
-            </Form>
-
-            {this.state.isShorten ? (
-              <InputGroup className="mb-3">
-                <Form.Control
-                  value={this.state.link_shorten}
-                  type="shorten-link"
-                  name="link_shorten"
-                  disabled
-                />
-                <InputGroup.Append>
-                  <CopyToClipboard
-                    text={this.state.link_shorten}
-                    onCopy={this.copyToClipboard}
-                  >
-                    <Button
-                      variant="outline-primary"
-                      type="copy"
-                      disabled={this.state.isLoading}
-                    >
-                      Copy
-                    </Button>
-                  </CopyToClipboard>
-                </InputGroup.Append>
-              </InputGroup>
-            ) : null}
+            <ListGroup>
+              {this.data.map((i) => {
+                return (
+                  <ListGroup.Item>
+                    <a href={`/topic/${i.topic_id}`}>{i.topic_header}</a>
+                    <div>
+                      <span className="username">{i.topic_created_user} </span>
+                      <span className="created_datetime">
+                        {i.topic_created_datetime.toLocaleString()}
+                      </span>
+                    </div>
+                  </ListGroup.Item>
+                );
+              })}
+            </ListGroup>
           </div>
         </div>
       </div>

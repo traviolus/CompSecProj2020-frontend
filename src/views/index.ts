0d980f7d5d4f1a7a4460698c83f7ac53300df5c0
Melvin Macaranas <greatwinblowaway@gmail.com>
2020-11-10 17:50:03 +0700

    done topic

--- src/views/index.ts
+++ src/views/index.ts
@@ -2,6 +2,6 @@ import Home from "views/Home";
 import Signin from "views/Signin";
 import Signup from "views/Signup";
 import Post from "views/Post";
-import ViewPost from "views/ViewPost";
+import ViewTopic from "views/ViewTopic";
 
-export { Home, Signin, Signup, Post, ViewPost };
+export { Home, Signin, Signup, Post, ViewTopic };
